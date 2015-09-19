7bb3ea0ca8403f280cd9bdca20309ca031b1e2df
masataka_kawashita <masa.tp@gmail.com>
2015-09-19 04:38:17 +0000

    edit Account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -14,12 +16,26 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user= User.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+     redirect_to current_user , notice: 'Account Updated.'
+    else
+     render 'edit'
+    end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation,:region,:profile)
+  end
+  
+  def set_user
+     @user= User.find(params[:id])
   end
 end
---
+++ db/migrate/20150919042540_add_to_users.rb
@@ -0,0 +1,6 @@
+class AddToUsers < ActiveRecord::Migration
+  def change
+     add_column :users, :region, :string
+     add_column :users, :profile, :string
+  end
+end
