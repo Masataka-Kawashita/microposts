c3db703fdbeed8ca5d3879d70c1a94450b216edd
masataka_kawashita <masa.tp@gmail.com>
2015-09-22 13:46:12 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,35 @@ class User < ActiveRecord::Base
                       format:{with: VALID_EMAIL_REGEX},
                       uniqueness:{case_sensitive: false}
     
+    validates :region,length: {maximum:255}
+    validates :profile,length: {maximum:255}
+    
     has_secure_password
     
     has_many :microposts
+    
+    has_many :following_relationships,class_name: "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent: :destroy
+
+    has_many :following_users,through: :following_relationships,source: :followed
+    
+    has_many :follower_relationships,class_name: "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent: :destroy
+    
+    has_many :follower_users, through: :follower_relationships,source: :follower
+    
+    def follow(other_user)
+        following_relationships.create(followed_id: other_user.id)
+    end
+    
+    def unfollow(other_user)
+        following_relationships.find_by(followed_id: other_user.id).destroy
+    end
+    
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
     resources :sessions,only:[:new,:create,:destroy]
     
     resources :microposts
+    
+    resources :relationships,only:[:create,:destroy]
 end
